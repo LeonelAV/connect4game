f384ab55bd3d7d539243113bc04a4fa0d25c12f3
LeonelAV <meirinho_leonel@hotmail.com>
2017-12-20 17:51:35 +0100

    Adding game state - step 3

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,14 @@ import Board from './Board';
 
 class Game extends Component {
   constructor(props){
-    super(props)
+    super(props);
+
+    var cells = []
+    for(let i=0; i<6; i++){
+      cells.push(new Array(7).fill(0))
+    }
+
+    this.state = {player: false, cells:cells, winner:0}
   }
 
   render() {

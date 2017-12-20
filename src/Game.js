27d3766e8ab1fa4c480a5ebcb9f6496405cd7d37
LeonelAV <meirinho_leonel@hotmail.com>
2017-12-20 17:33:08 +0100

    Game Component - step 2.5

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import Circle from './Circle';
 import Board from './Board';
 
 class Game extends Component {
+  constructor(props){
+    super(props)
+  }
+
   render() {
     return (
       <div>
+        <h1>Blacks Turn</h1>
         <Board />
+        <button>Restart</button>
       </div>
     );
   }

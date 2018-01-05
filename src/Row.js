b4eb7c4025089e836ce16a11370c497fd2e073bb
LeonelAV <meirinho_leonel@hotmail.com>
2018-01-05 15:06:35 +0100

    delete and prettify code

--- src/Row.js
+++ src/Row.js
@@ -5,10 +5,12 @@ function Row(props){
   var style = {
     display: "flex"
   }
+
   var cells = []
   for (let i=0; i < 7; i++){
     cells.push(<Cell key ={i} cell={props.cells[i]} row = {props.row} col={i} handleClick={props.handleClick} />)
   }
+
   return (
     <div style = {style}>
     {cells}
